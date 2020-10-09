672a1095c25defe5f0ed8f6bfb887f24d244cc0c
Bret Beheim <beheim@gmail.com>
2020-10-09 16:09:06 +0200

    2008-05-20 - more strategy experimentation

---
+++ strategies/stdDevTriggermean125.m
@@ -0,0 +1,29 @@
+%stdevTrigger (mean+1.25stdev)
+
+function [move, myRepertoire] = stdDevTrigger(roundsAlive, myRepertoire, myHistory) 
+
+if roundsAlive>1; %if this isn’t my first or second round… 
+    meanPayoffs = mean(myRepertoire(2,:)); %mean payoffs from Repertoire
+    stdPayoff=std(myRepertoire(2,:)); %Std payoff from Repertoire
+    lastPayoff=myHistory(4,roundsAlive);
+    
+    if lastPayoff > meanPayoffs+1.25*stdPayoff %if lastPayoff at least as good as a stdv above myMeanPayoff then EXPLOIT 
+        move=myHistory(3,roundsAlive);
+    else %otherwise 
+        if rand < 0.5
+            move = -1; % 50% of the time innovate
+        else
+            move = 0; % 50% otherwise observe
+        end
+    end
+
+elseif roundsAlive>0; %if this is my second round…
+    if rand < 0.5
+        move = myRepertoire(1,1); % half the time exploit
+    else
+        move = -1; %half the time innovate
+    end
+
+else
+    move = -1; %if this is my first round, then INNOVATE
+end 
---
+++ strategies/stdDevTriggermedian15.m
@@ -0,0 +1,29 @@
+%stdevTrigger (median+1.5stdev)
+
+function [move, myRepertoire] = stdDevTrigger(roundsAlive, myRepertoire, myHistory) 
+
+if roundsAlive>1; %if this isn’t my first or second round… 
+    medianPayoffs = median(myRepertoire(2,:)); %median payoffs from Repertoire
+    stdPayoff=std(myRepertoire(2,:)); %Std payoff from Repertoire
+    lastPayoff=myHistory(4,roundsAlive);
+    
+    if lastPayoff > medianPayoffs+1.50*stdPayoff %if lastPayoff at least as good as a stdv above medianPayoff then EXPLOIT 
+        move=myHistory(3,roundsAlive);
+    else %otherwise 
+        if rand < 0.5
+            move = -1; % 50% of the time innovate
+        else
+            move = 0; % 50% otherwise observe
+        end
+    end
+
+elseif roundsAlive>0; %if this is my second round…
+    if rand < 0.5
+        move = myRepertoire(1,1); % half the time exploit
+    else
+        move = -1; %half the time innovate
+    end
+
+else
+    move = -1; %if this is my first round, then INNOVATE
+end 
